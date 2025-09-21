b420d03f0764c075d191fa593e4ca06ada23a6c5
Santiago2209-03 <202260471@ucc.mx>
2025-09-20 18:47:03 -0600

    Add files via upload

---
+++ demo4Tests/demo4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  demo4Tests.swift
+//  demo4Tests
+//
+//  Created by Santiago Barradas on 13/08/25.
+//
+
+import Testing
+@testable import demo4
+
+struct demo4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
